79cd167f4e183684baa5e0bf55271d46912d5b85
Guilherme Rezende <guilhermebr@gmail.com>
2016-05-21 12:07:26 -0300

    Refactor faqs app

--- apoio/faqs/views.py
+++ apoio/faqs/views.py
@@ -2,11 +2,12 @@
 from django.template.loader import get_template
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question, Answer
+from .models import Question, Answer, _question_status
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(
+        status=_question_status.APPROVED).order_by('pub_date')
     template = get_template('faqs/index.html')
     context = {
         'latest_question_list': latest_question_list,
@@ -17,10 +18,20 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
-        answer   = Answer.objects.get(pk=question_id)
+        answers   = Answer.objects.filter(question=question, accepted=False)
+        try:
+            accepted_answer = Answer.objects.get(question=question, accepted=True)
+        except:
+            accepted_answer = ""
+
+
+        print(answers)
     except Question.DoesNotExist:
         raise Http404("Essa pergunta não foi feita ou ainda não foi respondida.")
-    return render(request, 'faqs/detail.html', {'question': question, 'answer': answer})
+    return render(request, 'faqs/detail.html', {
+        'question': question,
+        'answers': answers,
+        'accepted_answer': accepted_answer})
 
 
 def results(request, question_id):
